f8c0cb72deae627559f03c93854f4ba4b371e2d3
null-laptop <170277151+senganull@users.noreply.github.com>
2026-02-10 15:59:22 +0900

    fxmanifestを実装

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'senganull'
+version '1.0.0'
+
+ui_page 'ui/index.html'
+
+client_scripts {
+    'client/cl_main.lua'
+}
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/script.js'
+}
